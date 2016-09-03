d7cd4bf9b14db104cef37562cf1973e0f80f20a4
Matt Hamlin <hamlim@outlook.com>
2016-09-03 17:59:48 -0400

    boom web pack

---
+++ app.js
@@ -0,0 +1,8 @@
+var css = require("!style!css!sass!./assets/css/src/main.scss");
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import View from './assets/js/components'
+// import App from './assets/js/app'
+
+ReactDOM.render(<View />, document.getElementById('app'));
---
+++ assets/js/components.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default class View extends React.Component {
+	render() {
+		return (
+			<h1>Test</h1>
+		)
+	}
+}
